728162d08eb4e82947290b284a4a3a7abac77873
Richard Barnes <rbarnes@umn.edu>
2018-12-04 22:15:16 -0800

    Make sure to add water from children to their parents while running Overflow()

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -337,6 +337,16 @@ void Overflow(
   if(current_depression==OCEAN)
     return;
 
+  {
+    const int lchild = this_dep.lchild;
+    const int rchild = this_dep.rchild;
+
+    //Add child's water to this depression
+    if(lchild!=NO_VALUE && deps.at(lchild).water_vol==deps.at(lchild).dep_vol)
+      this_dep.water_vol += deps.at(lchild).water_vol;
+    if(rchild!=NO_VALUE && deps.at(rchild).water_vol==deps.at(rchild).dep_vol)
+      this_dep.water_vol += deps.at(rchild).water_vol;
+  }
 
   //Each depression has an associated dep_vol. This is the TOTAL volume of the
   //meta-depression including all of its children. This property answers the
