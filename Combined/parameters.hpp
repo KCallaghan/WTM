e26ab962fad218e97b3edccb4daed0567dc4cdb7
Andy Wickert <andy@northernwidget.com>
2020-05-12 01:13:51 -0500

    Informative types; wtd variable names

--- Combined/parameters.hpp
+++ Combined/parameters.hpp
@@ -7,55 +7,57 @@
 
 const std::string UNINIT_STR = "uninitialized";
 
-class Parameters {
- public:
-  Parameters(const std::string config_file);
+class Parameters{
+public:
+    Parameters(const std::string config_file);
 
-  int iterations = -1;
-  int maxiter    = -1;
+    int32_t iterations = -1;
+    int32_t maxiter    = -1;
 
-  std::string surfdatadir  = UNINIT_STR;
-  std::string region       = UNINIT_STR;
-  std::string run_type     = UNINIT_STR;
-  std::string time_start   = UNINIT_STR;
-  std::string time_end     = UNINIT_STR;
-  std::string textfilename = UNINIT_STR;
-  std::string outfilename  = UNINIT_STR;
+    std::string surfdatadir  = UNINIT_STR;
+    std::string region       = UNINIT_STR;
+    std::string run_type     = UNINIT_STR;
+    std::string time_start   = UNINIT_STR;
+    std::string time_end     = UNINIT_STR;
+    std::string textfilename = UNINIT_STR;
+    std::string outfilename  = UNINIT_STR;
 
-  int cells_per_degree = -1;
+    int16_t cells_per_degree = -1;
 
-  const double UNDEF  = -1.0e7;
+    const float64_t UNDEF  = -1.0e7;
 
-  bool infiltration_on;
-  
-  double southern_edge        = std::numeric_limits<double>::signaling_NaN();
-  double deltat               = std::numeric_limits<double>::signaling_NaN();
-  double cellsize_n_s_metres  = std::numeric_limits<double>::signaling_NaN();
-  float  infiltration         = 0.0;
-  int    cycles_done          = 0;
-  float  total_wtd_change     = 0.0;
-  float  wtd_mid_change       = 0.0;
-  float  GW_wtd_change        = 0.0;
-  float  abs_total_wtd_change = 0.0;
-  float  abs_wtd_mid_change   = 0.0;
-  float  abs_GW_wtd_change    = 0.0;
-  float  infiltration_change  = 0.0;
-  float  surface_change       = 0.0;
-  int    total_cycles         = -1;
+    bool infiltration_on;
 
+    float64_t southern_edge         = std::numeric_limits<float64_t>\
+                                         ::signaling_NaN();
+    float64_t deltat                = std::numeric_limits<float64_t>\
+                                         ::signaling_NaN();
+    float64_t cellsize_n_s_metres   = std::numeric_limits<float64_t>\
+                                         ::signaling_NaN();
+    float32_t  infiltration         = 0.;
+    int32_t    cycles_done          = 0;
+    float32_t  total_wtd_change     = 0.;
+    float32_t  wtd_mid_change       = 0.;
+    float32_t  GW_wtd_change        = 0.;
+    float32_t  abs_total_wtd_change = 0.;
+    float32_t  abs_wtd_mid_change   = 0.;
+    float32_t  abs_GW_wtd_change    = 0.;
+    float32_t  infiltration_change  = 0.;
+    float32_t  surface_change       = 0.;
+    int32_t    total_cycles         = -1;
 
-  float N = 0.0;
-  float S = 0.0;
-  float E = 0.0;
-  float W = 0.0;
-  float me = 0.0;
+    // Water-table depths for transient_groundwater module
+    float32_t wtdN      = 0.;
+    float32_t wtdS      = 0.;
+    float32_t wtdE      = 0.;
+    float32_t wtdW      = 0.;
+    float32_t wtdCenter = 0.;
 
+    //Set for convenience within the code
+    int32_t ncells_x  = -1;
+    int32_t ncells_y  = -1;
 
-  //Set for convenience within the code
-  int ncells_x  = -1;
-  int ncells_y  = -1;
-
-  void print() const;
+    void print() const;
 };
 
 #endif
