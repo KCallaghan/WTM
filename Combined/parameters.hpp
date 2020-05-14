fb3b9ee341d4284c7a788c91af4ec3043d6725e3
Andy Wickert <andy@northernwidget.com>
2020-05-13 23:54:04 -0500

    typing; dummy initializer

--- Combined/parameters.hpp
+++ Combined/parameters.hpp
@@ -1,6 +1,7 @@
 #ifndef _parameters_hpp_
 #define _parameters_hpp_
 
+#include <stdint.h>
 #include <cmath>
 #include <limits>
 #include <string>
@@ -9,6 +10,7 @@ const std::string UNINIT_STR = "uninitialized";
 
 class Parameters{
 public:
+    Parameters();
     Parameters(const std::string config_file);
 
     int32_t iterations = -1;
@@ -24,27 +26,28 @@ public:
 
     int16_t cells_per_degree = -1;
 
-    const float64_t UNDEF  = -1.0e7;
+    //const double UNDEF  = -1.0e7;
+    double UNDEF  = -1.0e7;
 
     bool infiltration_on;
 
-    float64_t southern_edge         = std::numeric_limits<float64_t>\
+    double     southern_edge       = std::numeric_limits<double>\
                                          ::signaling_NaN();
-    float64_t deltat                = std::numeric_limits<float64_t>\
+    double     deltat              = std::numeric_limits<double>\
                                          ::signaling_NaN();
-    float64_t cellsize_n_s_metres   = std::numeric_limits<float64_t>\
+    double    cellsize_n_s_metres  = std::numeric_limits<double>\
                                          ::signaling_NaN();
-    float32_t  infiltration         = 0.;
-    int32_t    cycles_done          = 0;
-    float32_t  total_wtd_change     = 0.;
-    float32_t  wtd_mid_change       = 0.;
-    float32_t  GW_wtd_change        = 0.;
-    float32_t  abs_total_wtd_change = 0.;
-    float32_t  abs_wtd_mid_change   = 0.;
-    float32_t  abs_GW_wtd_change    = 0.;
-    float32_t  infiltration_change  = 0.;
-    float32_t  surface_change       = 0.;
-    int32_t    total_cycles         = -1;
+    float     infiltration         = 0.;
+    int32_t   cycles_done          = 0;
+    float     total_wtd_change     = 0.;
+    float     wtd_mid_change       = 0.;
+    float     GW_wtd_change        = 0.;
+    float     abs_total_wtd_change = 0.;
+    float     abs_wtd_mid_change   = 0.;
+    float     abs_GW_wtd_change    = 0.;
+    float     infiltration_change  = 0.;
+    float     surface_change       = 0.;
+    int32_t   total_cycles         = -1;
 
     //Set for convenience within the code
     int32_t ncells_x  = -1;
