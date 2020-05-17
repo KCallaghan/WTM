cab9f37c9f8c0031d9340be16c5fc54d684ebfa5
Andy Wickert <andy@northernwidget.com>
2020-05-17 11:18:06 -0500

    5 possibilities for porosity in stencil

--- Combined/transient_groundwater.cpp
+++ Combined/transient_groundwater.cpp
@@ -96,7 +96,7 @@ double FanDarcyGroundwater::computeMaxStableTimeStep(Parameters &params, ArrayPa
     float *PhiArray[5] = { &arp.porosity(x, y),
                              &arp.porosity(x+1, y), &arp.porosity(x-1, y),
                              &arp.porosity(x, y+1), &arp.porosity(x, y-1) };
-    float PhiMin = computeArrayMin( PhiArray, 4 ); // Minimum porosity
+    float PhiMin = computeArrayMin( PhiArray, 5 ); // Minimum porosity
     // Porosity is a linear amplifier of WTD change, and it amplifies change
     // in both the giving and receiving cells.
     // Amplification goes as 1 / phi.
