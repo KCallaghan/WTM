dc0214dc44d43a834949c8cc53f5c432c4ebd8dd
KCallaghan <calla350@umn.edu>
2020-09-22 17:05:15 -0500

    Add recharge only to land cells.

--- Combined/add_recharge.hpp
+++ Combined/add_recharge.hpp
@@ -6,18 +6,18 @@ void add_recharge(const int x, const int y, Parameters &params, ArrayPack &arp){
 
   double rech_change = arp.rech(x,y)/31536000. * params.deltat;
 
-  if(arp.wtd(x,y) >= 0){  //all the recharge will occur above the land surface; don't worry about porosity. 
+  if(arp.wtd(x,y) >= 0 && arp.land_mask(i) == 1){  //all the recharge will occur above the land surface; don't worry about porosity.
     arp.wtd(x,y) += rech_change;
     if(arp.wtd(x,y) < 0)
-      arp.wtd(x,y) = 0; //however, if we had evaporation, don't evaporate more surface water than is available. 
+      arp.wtd(x,y) = 0; //however, if we had evaporation, don't evaporate more surface water than is available.
   }
-  else if(rech_change>0){ //at least some of the water will be added into the ground, so we need to think about porosity. If rech_change was < 0 we don't add it here since there is no surface water available to evaporate.
+  else if(rech_change>0 && arp.land_mask(i) == 1){ //at least some of the water will be added into the ground, so we need to think about porosity. If rech_change was < 0 we don't add it here since there is no surface water available to evaporate.
     double GW_space = -arp.wtd(x,y) * arp.porosity(x,y);  //if wtd is negative, this is the amount of above-ground equivalent recharge that can be accommodated below ground.
-    if(GW_space > rech_change){ //all of the recharge will be below the ground; GW will not be completely filled. 
+    if(GW_space > rech_change){ //all of the recharge will be below the ground; GW will not be completely filled.
       arp.wtd(x,y) += rech_change / arp.porosity(x,y);
     }
-    else{  //we will have some below-ground and some above-ground water added. 
-      arp.wtd(x,y) = rech_change - GW_space; //some is used up in GW space and the remainder goes above the ground. 
+  else if(arp.land_mask(i) == 1){  //we will have some below-ground and some above-ground water added.
+      arp.wtd(x,y) = rech_change - GW_space; //some is used up in GW space and the remainder goes above the ground.
     }
   }
 }
@@ -30,24 +30,24 @@ void add_recharge(const int x, const int y, Parameters &params, ArrayPack &arp){
 //  double GW_portion = 0;
 //
 //
-//  if(arp.wtd(x,y) >= 0){  //all the recharge will occur above the land surface; don't worry about porosity. 
+//  if(arp.wtd(x,y) >= 0){  //all the recharge will occur above the land surface; don't worry about porosity.
 //    arp.wtd(x,y) += arp.rech(x,y)/31536000. * params.deltat;
 //    if(arp.wtd(x,y) < 0)
-//      arp.wtd(x,y) = 0; //however, if we had evaporation, don't evaporate more surface water than is available. 
+//      arp.wtd(x,y) = 0; //however, if we had evaporation, don't evaporate more surface water than is available.
 //  }
-//  else if(volume_change>0){ //at least some of the water will be added into the ground, so we need to think about porosity. 
+//  else if(volume_change>0){ //at least some of the water will be added into the ground, so we need to think about porosity.
 //    arp.wtd(x,y) = arp.fdepth(x,y) * log(exp(arp.wtd(x,y) / arp.fdepth(x,y)) \
 //          + volume_change / ( arp.cell_area[y] * arp.porosity(x,y) * arp.fdepth(x,y)) );
 //
 //    if(arp.wtd(x,y) > 0 || (exp(arp.wtd(x,y)/arp.fdepth(x,y)) <  (volume_change / (arp.cell_area[y] * arp.porosity(x,y) * arp.fdepth(x,y)))  ) ){ //part of the change is below the ground, part above. So we need to calculate how much of the water was used up in the ground, i.e. the portion between the starting wtd and 0.
 //      GW_portion = -arp.cell_area[y] * arp.porosity(x,y) * arp.fdepth(x,y) * \
 //                       (exp(arp.wtd(x,y) / arp.fdepth(x,y)) - 1);
-//                       //this is the volume of water used up in filling in the ground. 
-//                       //volume_change - GW_portion is left over to fill surface water. 
+//                       //this is the volume of water used up in filling in the ground.
+//                       //volume_change - GW_portion is left over to fill surface water.
 //      arp.wtd(x,y) = ((volume_change - GW_portion) / arp.cell_area[y]);
 //    }
 //  }
 //}
 //
 //
-//  
+//
