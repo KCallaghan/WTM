92d1bf0282d2e94bb4db179626e85a77e5e1fb64
KCallaghan <calla350@umn.edu>
2020-09-25 16:54:54 -0500

    Update Arraypack checks

--- Combined/ArrayPack.cpp
+++ Combined/ArrayPack.cpp
@@ -2,22 +2,20 @@
 #include <cassert>
 
 void ArrayPack::check() const {
-  assert( topo.width()==ksat.         width() && topo.height()==ksat.         height() );
-  assert( topo.width()==land_mask.    width() && topo.height()==land_mask.    height() );
-  assert( topo.width()==wtd.          width() && topo.height()==wtd.          height() );
-  assert( topo.width()==fdepth.       width() && topo.height()==fdepth.       height() );
- // assert( topo.width()==temp.         width() && topo.height()==temp.         height() );
- // assert( topo.width()==slope.        width() && topo.height()==slope.        height() );
-  assert( topo.width()==precip.       width() && topo.height()==precip.       height() );
-  assert( topo.width()==fdepth.       width() && topo.height()==fdepth.       height() );
-  assert( topo.width()==starting_evap.width() && topo.height()==starting_evap.height() );
- // assert( topo.width()==relhum.       width() && topo.height()==relhum.       height() );
-  assert( topo.width()==head.         width() && topo.height()==head.         height() );
- // assert( topo.width()==evap.         width() && topo.height()==evap.         height() );
- // assert( topo.width()==e_sat.        width() && topo.height()==e_sat.        height() );
- // assert( topo.width()==e_a.          width() && topo.height()==e_a.          height() );
- // assert( topo.width()==surface_evap. width() && topo.height()==surface_evap. height() );
-  assert( topo.width()==runoff.       width() && topo.height()==runoff.       height() );
+  assert( topo.width()==ksat.           width() && topo.height()==ksat.           height() );
+  assert( topo.width()==land_mask.      width() && topo.height()==land_mask.      height() );
+  assert( topo.width()==wtd.            width() && topo.height()==wtd.            height() );
+  assert( topo.width()==fdepth.         width() && topo.height()==fdepth.         height() );
+  assert( topo.width()==slope.          width() && topo.height()==slope.          height() );
+  assert( topo.width()==precip.         width() && topo.height()==precip.         height() );
+  assert( topo.width()==rech.           width() && topo.height()==rech.           height() );
+  assert( topo.width()==starting_evap.  width() && topo.height()==starting_evap.  height() );
+  assert( topo.width()==transmissivity. width() && topo.height()==transmissivity. height() );
+  assert( topo.width()==head.           width() && topo.height()==head.           height() );
+  assert( topo.width()==porosity.       width() && topo.height()==porosity.       height() );
+  assert( topo.width()==winter_temp.    width() && topo.height()==winter_temp.    height() );
+  assert( topo.width()==open_water_evap.width() && topo.height()==open_water_evap.height() );
+  assert( topo.width()==runoff.         width() && topo.height()==runoff.         height() );
 
   if(fdepth_end.size()>0){
     assert( topo.width()==fdepth_end.width()  && topo.height()==fdepth_end.   height()   );
