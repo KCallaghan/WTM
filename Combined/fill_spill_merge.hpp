4358411957a5d44a5f7254c5c4dc8e95563b1d0f
KCallaghan <calla350@umn.edu>
2020-10-06 17:09:14 -0500

    Working on unit tests

--- Combined/fill_spill_merge.hpp
+++ Combined/fill_spill_merge.hpp
@@ -1431,10 +1431,11 @@ static void FillAFullDepression(
     //for each one, check through all of the cells contained within the
     //depression with that label (only leaf depressions store this information)
     for(const auto cell: deps.at(label).my_cells){
+      std::cout<<"fill label "<<label<<" cell "<<cell<<std::endl;
       //if the cell is lower than the outlet elevation, it is affected
       //by the depression fill on this level, so we update the wtd in this cell.
       if(arp.topo(cell) > deps.at(stdi.top_label).out_elev)
-        break;
+        std::cout<<"wtf"<<std::endl;
       else
         arp.wtd(cell) = deps.at(stdi.top_label).out_elev - arp.topo(cell);
     }
--- Combined/test_kerry_FSM.cpp
+++ Combined/test_kerry_FSM.cpp
@@ -138,7 +138,6 @@ TEST_CASE("Determine water level"){
 
 
 
-
 TEST_CASE("Backfill Depression"){
   const Array2D<double> topo = {
       {-9, -9, -9, -9, -9, -9, -9, -9, -9, -9},
@@ -182,70 +181,173 @@ TEST_CASE("Backfill Depression"){
 
 
 
+TEST_CASE("Add recharge"){
+//double add_recharge(const double deltat, const double my_rech, double my_wtd, const int my_mask, const double my_porosity){
 
+  //positive wtd, positive rech
+  double new_wtd = add_recharge(31536000., 1., 1., 1, 0.4);
+  CHECK(new_wtd==2);
 
+  //positive wtd, negative rech that still ends positive
+  new_wtd = add_recharge(31536000., -0.5, 1., 1, 0.4);
+  CHECK(new_wtd==0.5);
 
+  //positive wtd, negative rech that would end negative so gets corrected to 0
+  new_wtd = add_recharge(31536000., -2., 1., 1, 0.4);
+  CHECK(new_wtd==0);
 
+  //negative wtd, negative rech: wtd should not change
+  new_wtd = add_recharge(31536000., -1., -1., 1, 0.4);
+  CHECK(new_wtd==-1);
 
+  //negative wtd, positive rech, wtd stays negative
+  new_wtd = add_recharge(31536000., 1., -10., 1, 0.4);
+  CHECK(new_wtd==-7.5);
 
+  //negative wtd, positive rech, wtd becomes positive
+  new_wtd = add_recharge(31536000., 1., -1., 1, 0.4);
+  CHECK(new_wtd==0.6);
 
-
-void RandomizedHeavyFloodingVsPriorityFlood(const int count, const int min_size, const int max_size){
-  #pragma omp parallel for
-  for(int i=0;i<count;i++){
-    std::stringstream oss;
-
-    ArrayPack arp;
-    Parameters params;
-
-    Array2D<double> dem;
-
-    #pragma omp critical
-    {
-      oss<<gen;
-      dem = random_terrain(gen, min_size, max_size);
-      std::cerr<<"Randomized Heavy Flooding vs Priority-Flood #"<<i<<std::endl;
-    }
-
-    arp.topo = dem;
-    arp.label = Array2D<dh_label_t> (arp.topo.width(), arp.topo.height(), NO_DEP );
-    arp.final_label = Array2D<dh_label_t> (arp.topo.width(), arp.topo.height(), NO_DEP );
-    arp.flowdirs = Array2D<flowdir_t>  (arp.topo.width(), arp.topo.height(), NO_FLOW);
-
-    arp.cell_area = std::vector<double> (arp.topo.height(), 1.);
-
-
-    arp.topo.setEdges(-1);
-    arp.label.setEdges(OCEAN);
-
-    auto deps = GetDepressionHierarchy<double,Topology::D8>(arp.topo, arp.cell_area,arp.label,arp.final_label, arp.flowdirs);
-
-    //wtd with a *lot* of initial surface water
-    Array2D<double> wtd(arp.topo.width(), arp.topo.height(), 100);
-
-    try {
-      FillSpillMerge(params, deps, arp);
-    } catch (const std::exception &e) {
-      std::cerr<<"FillSpillMerge failed because of \""<<e.what()<<"\" with width = "<<arp.topo.width()<<" height = "<<arp.topo.height()<<" state = "<<oss.str()<<std::endl;
-      throw e;
-    }
-
-    for(auto i=arp.topo.i0(); i<arp.topo.size(); i++){
-      if(!arp.topo.isNoData(i))
-        arp.topo(i) += wtd(i);
-    }
-
-    auto comparison_dem = arp.topo;
-    PriorityFlood_Zhou2016(comparison_dem);
-
-    CHECK_MESSAGE(MaxArrayDiff(comparison_dem,arp.topo)<1e-6, "Randomized Heavy Flooding vs Priority-Flood failed with width = "+std::to_string(arp.topo.width())+" height = "+std::to_string(arp.topo.height())+" state = " + oss.str());
-  }
 }
 
-TEST_CASE("Randomized Heavy Flooding vs Priority-Flood"){
-  RandomizedHeavyFloodingVsPriorityFlood(number_of_small_tests,  10,  30);
-  RandomizedHeavyFloodingVsPriorityFlood(number_of_large_tests, 100, 300);
+
+TEST_CASE("Fill a full depression"){
+  SubtreeDepressionInfo my_stdi;
+  my_stdi.my_labels.emplace(1);
+  my_stdi.leaf_label = 1;
+  my_stdi.top_label = 1;
+
+  ArrayPack arp;
+
+  arp.wtd = Array2D<double>(10,10,0);
+  arp.topo.resize(arp.wtd.width(),arp.wtd.height(),2);
+  arp.topo.setEdges(-9);
+  arp.topo(4,2) = 1;
+  arp.topo(4,3) = 1;
+  arp.topo(4,4) = 1;
+  arp.topo(4,5) = 1;
+  arp.topo(4,6) = 1;
+
+
+
+  std::cout<<"topo"<<std::endl;
+  arp.topo.printAll();
+
+  const Array2D<double> dem = {
+      {-9,-9,-9,-9,-9,-9,-9,-9,-9, -9},
+      {-9, 2, 2, 2, 2, 2, 2, 2, 2, -9},
+      {-9, 2, 2, 2, 1, 2, 2, 2, 2, -9},
+      {-9, 2, 2, 2, 1, 2, 2, 2, 2, -9},
+      {-9, 2, 2, 2, 1, 2, 2, 2, 2, -9},
+      {-9, 2, 2, 2, 1, 2, 2, 2, 2, -9},
+      {-9, 2, 2, 2, 1, 2, 2, 2, 2, -9},
+      {-9, 2, 2, 2, 2, 2, 2, 2, 2, -9},
+      {-9, 2, 2, 2, 2, 2, 2, 2, 2, -9},
+      {-9,-9,-9,-9,-9,-9,-9,-9,-9, -9},
+  };
+
+ //not sure why passing dem to GetDepressionHierarchy below works, and passing arp.topo doesn't?? So weird
+  arp.wtd = Array2D<double> (arp.topo.width(), arp.topo.height(), 0. );
+  arp.label = Array2D<dh_label_t> (arp.topo.width(), arp.topo.height(), NO_DEP );
+  arp.label.setEdges(OCEAN);
+
+  arp.final_label = Array2D<dh_label_t> (arp.topo.width(), arp.topo.height(), NO_DEP );
+  arp.final_label.setEdges(OCEAN);
+
+  arp.flowdirs = Array2D<flowdir_t>  (arp.topo.width(), arp.topo.height(), NO_FLOW);
+  arp.cell_area = std::vector<double> (arp.topo.height(), 1.);
+
+  const Array2D<double> wtd_good = {
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  };
+
+  auto DH = GetDepressionHierarchy<double,Topology::D8>(dem, arp.cell_area, arp.label, arp.final_label, arp.flowdirs);
+
+  FillAFullDepression(my_stdi, DH, arp);
+
+std::cout<<"labels"<<std::endl;
+  arp.label.printAll();
+
+std::cout<<"wtd"<<std::endl;
+  arp.wtd.printAll();
+
+
+
+
+  CHECK(ArrayValuesEqual(arp.wtd,wtd_good));
+
 }
+
+
+
+
+
+//void RandomizedHeavyFloodingVsPriorityFlood(const int count, const int min_size, const int max_size){
+//  #pragma omp parallel for
+//  for(int i=0;i<count;i++){
+//    std::stringstream oss;
+//
+//    ArrayPack arp;
+//    Parameters params;
+//    params.textfilename = "test.txt";
+//
+//    Array2D<double> dem;
+//
+//    #pragma omp critical
+//    {
+//      oss<<gen;
+//      dem = random_terrain(gen, min_size, max_size);
+//      std::cerr<<"Randomized Heavy Flooding vs Priority-Flood #"<<i<<std::endl;
+//    }
+//
+//    arp.topo = dem;
+//    arp.label = Array2D<dh_label_t> (arp.topo.width(), arp.topo.height(), NO_DEP );
+//    arp.final_label = Array2D<dh_label_t> (arp.topo.width(), arp.topo.height(), NO_DEP );
+//    arp.flowdirs = Array2D<flowdir_t>  (arp.topo.width(), arp.topo.height(), NO_FLOW);
+//
+//    arp.cell_area = std::vector<double> (arp.topo.height(), 1.);
+//
+//
+//    arp.topo.setEdges(-1);
+//    arp.label.setEdges(OCEAN);
+//
+//    auto deps = GetDepressionHierarchy<double,Topology::D8>(arp.topo, arp.cell_area,arp.label,arp.final_label, arp.flowdirs);
+//
+//    //wtd with a *lot* of initial surface water
+//    Array2D<double> wtd(arp.topo.width(), arp.topo.height(), 100);
+//
+//    try {
+//      FillSpillMerge(params, deps, arp);
+//    } catch (const std::exception &e) {
+//      std::cerr<<"FillSpillMerge failed because of \""<<e.what()<<"\" with width = "<<arp.topo.width()<<" height = "<<arp.topo.height()<<" state = "<<oss.str()<<std::endl;
+//      throw e;
+//    }
+//
+//    for(auto i=arp.topo.i0(); i<arp.topo.size(); i++){
+//      if(!arp.topo.isNoData(i))
+//        arp.topo(i) += wtd(i);
+//    }
+//
+//    auto comparison_dem = arp.topo;
+//    PriorityFlood_Zhou2016(comparison_dem);
+//
+//    CHECK_MESSAGE(MaxArrayDiff(comparison_dem,arp.topo)<1e-6, "Randomized Heavy Flooding vs Priority-Flood failed with width = "+std::to_string(arp.topo.width())+" height = "+std::to_string(arp.topo.height())+" state = " + oss.str());
+//  }
+//}
+//
+//TEST_CASE("Randomized Heavy Flooding vs Priority-Flood"){
+//  RandomizedHeavyFloodingVsPriorityFlood(number_of_small_tests,  10,  30);
+//  RandomizedHeavyFloodingVsPriorityFlood(number_of_large_tests, 100, 300);
+//}
 
 
 
