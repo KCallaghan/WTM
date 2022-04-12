6a734ff00d0f80d3205813bddb6ef582986f9374
KCallaghan <calla350@umn.edu>
2022-04-12 13:36:33 -0400

    apply changes to calculation of cell areas and latitudes to the run_dephier file

--- src/run_dephier.cpp
+++ src/run_dephier.cpp
@@ -58,53 +58,40 @@ int main(int argc, char** argv) {
 
   rd::Array2D<rd::flowdir_t> flowdirs(params.ncells_x, params.ncells_y, rd::NO_FLOW);  // No cells flow anywhere
 
-  const float earth_radius = 6371000.;  // metres
+  constexpr float earth_radius = 6371000.;  // metres
+  // Radius * Pi = Distance from N to S pole
+  // Distance / 180 = Meters / degree latitude
+  const auto meters_per_degree = earth_radius * DEG_TO_RAD;
 
   // distance between lines of latitude is a constant.
-  params.cellsize_n_s_metres = (earth_radius * DEG_TO_RAD) / params.cells_per_degree;
+  params.cellsize_n_s_metres = meters_per_degree / params.cells_per_degree;
 
   // initialise some arrays
-  arp.latitude_radians.resize(params.ncells_y);
-  // the latitude of each row of cells
-  arp.cellsize_e_w_metres.resize(params.ncells_y);
   // size of a cell in the east-west direction at the centre of the cell (metres)
-  arp.cell_area.resize(params.ncells_y);
+  arp.cellsize_e_w_metres.resize(params.ncells_y);
   // cell area (metres squared)
+  arp.cell_area.resize(params.ncells_y);
 
-  for (unsigned int j = 0; j < arp.latitude_radians.size(); j++) {
-    // latitude at the centre of a cell:
-    arp.latitude_radians[j] = (float(j) / params.cells_per_degree + params.southern_edge) * DEG_TO_RAD;
+  for (unsigned int j = 0; j < params.ncells_y; j++) {
     // southern edge of the domain in degrees, plus the number of cells up
     // from this location/the number of cells per degree, converted to radians.
-
-    // cells_per_degree = 120, there are this many 30 arc-second pieces in
-    // one degree. (or however many pieces per degree the user has)
-    // j/cells_per_degree gives the number  of degrees up from the southern edge,
-    // add southern_edge since the southern edge may not be at 0 latitude.
-    //*pi/180 to convert to radians.
-    // latitude_radians is now the latitude in radians.
-
     // latitude at the southern edge of a cell (subtract half a cell):
-    double latitude_radians_S = ((float(j) - 0.5) / params.cells_per_degree + params.southern_edge) * DEG_TO_RAD;
+    const double latitude_radians_S = ((float(j))      / params.cells_per_degree + params.southern_edge) * DEG_TO_RAD;
     // latitude at the northern edge of a cell (add half a cell):
-    double latitude_radians_N = ((float(j) + 0.5) / params.cells_per_degree + params.southern_edge) * DEG_TO_RAD;
+    const double latitude_radians_N = ((float(j) + 1.) / params.cells_per_degree + params.southern_edge) * DEG_TO_RAD;
+
+   // distance at the northern edge of the cell for the given latitude:
+    double cellsize_e_w_metres_N = params.cellsize_n_s_metres * std::cos(latitude_radians_N);
+    // distance at the southern edge of the cell for the given latitude:
+    double cellsize_e_w_metres_S = params.cellsize_n_s_metres * std::cos(latitude_radians_S);
 
     // distance between lines of longitude varies with latitude.
     // This is the distance at the centre of a cell for a given latitude:
-    arp.cellsize_e_w_metres[j] =
-        earth_radius * std::cos(arp.latitude_radians[j]) * DEG_TO_RAD / params.cells_per_degree;
-
-    // distance at the northern edge of the cell for the given latitude:
-    double cellsize_e_w_metres_N = earth_radius * std::cos(latitude_radians_N) * DEG_TO_RAD / params.cells_per_degree;
-    // distance at the southern edge of the cell for the given latitude:
-    double cellsize_e_w_metres_S = earth_radius * std::cos(latitude_radians_S) * DEG_TO_RAD / params.cells_per_degree;
+     arp.cellsize_e_w_metres[j] = (cellsize_e_w_metres_N + cellsize_e_w_metres_S) / 2.;
 
     // cell area computed as a trapezoid, using unchanging north-south distance,
     // and east-west distances at the northern and southern edges of the cell:
-    arp.cell_area[j] = params.cellsize_n_s_metres * (cellsize_e_w_metres_N + cellsize_e_w_metres_S) / 2;
-
-    // TODO(kcallaghan): see which, if any, arrays can be cleared after use to free up memory.
-    // How to do this?
+    arp.cell_area[j] = params.cellsize_n_s_metres * arp.cellsize_e_w_metres[j];
   }
 
   // Label the ocean cells. This is a precondition for using
