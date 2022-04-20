5587ea2dfa1ccd7efae5476e2fde86741756d000
Richard Barnes <rbarnes@umn.edu>
2022-04-20 13:56:26 -0700

    Clean up add_recharge.hpp

--- src/add_recharge.hpp
+++ src/add_recharge.hpp
@@ -14,14 +14,16 @@ inline double add_recharge(
     const bool count_recharge,
     Parameters& params) {
   double recharge_to_add = 0.;
+
   if (count_recharge) {
     params.total_added_recharge += my_rech * cell_area;
   }
+
   if (my_wtd >= 0) {  // all the recharge will occur above the land surface; don't worry about porosity.
     recharge_to_add = my_rech;
     if (my_wtd + recharge_to_add < 0) {
-      recharge_to_add =
-          -my_wtd;  // however, if we had evaporation, don't evaporate more surface water than is available.
+      // however, if we had evaporation, don't evaporate more surface water than is available.
+      recharge_to_add = -my_wtd;
       if (count_recharge) {
         params.total_added_recharge -= my_rech * cell_area;
       }
@@ -31,12 +33,15 @@ inline double add_recharge(
                              // water available to evaporate.
     const double GW_space = -my_wtd * my_porosity;  // if wtd is negative, this is the amount of above-ground equivalent
                                                     // recharge that can be accommodated below ground.
-    if (GW_space > my_rech) {  // all of the recharge will be below the ground; GW will not be completely filled.
+    if (GW_space > my_rech) {
+      // all of the recharge will be below the ground; GW will not be completely filled.
       recharge_to_add = my_rech / my_porosity;
-    } else {  // we will have some below-ground and some above-ground water added.
-      recharge_to_add =
-          my_rech - GW_space - my_wtd;  // some is used up in GW space and the remainder goes above the ground.
+    } else {
+      // we will have some below-ground and some above-ground water added.
+      // some is used up in GW space and the remainder goes above the ground.
+      recharge_to_add = my_rech - GW_space - my_wtd;
     }
   }
+
   return recharge_to_add;
 }
