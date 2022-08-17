abf1c5e79a7d07e61d320b5d4424a113e27085b4
KCallaghan <calla350@umn.edu>
2022-08-17 14:59:51 -0400

    tidying

--- src/DMDA_array_pack.cpp
+++ src/DMDA_array_pack.cpp
@@ -16,21 +16,6 @@ void populate_DMDA_array_pack(AppCtx& user_context, ArrayPack& arp, DMDA_Array_P
       dmdapack.ksat_vec[j][i]    = arp.ksat(i, j);
       dmdapack.porosity[j][i]    = arp.porosity(i, j);
       dmdapack.topo_vec[j][i]    = arp.topo(i, j);
-      //     std::cout<<" i "<<i<<" j "<<j<<" mask arp "<<arp.land_mask(i,j)<<" dmdapack
-      //     "<<dmdapack.mask[i][j]<<std::endl; std::cout<<" i "<<i<<" j "<<j<<" fdepth arp "<<arp.fdepth(i,j)<<"
-      //     dmdapack "<<dmdapack.fdepth_vec[i][j]<<std::endl; std::cout<<" i "<<i<<" j "<<j<<" ksat arp
-      //     "<<arp.ksat(i,j)<<" dmdapack "<<dmdapack.ksat_vec[i][j]<<std::endl; std::cout<<" i "<<i<<" j "<<j<<"
-      //     porosity arp "<<arp.porosity(i,j)<<" dmdapack "<<dmdapack.porosity[i][j]<<std::endl;
-    }
-  }
-
-  for (auto j = ys; j < ys + ym; j++) {
-    for (auto i = xs; i < xs + xm; i++) {
-      //   std::cout<<" i "<<i<<" j "<<j<<" at creation mask "    <<arp.land_mask(i,j)<<"
-      //   "<<dmdapack.mask[i][j]<<std::endl; std::cout<<" i "<<i<<" j "<<j<<" at creation fdepth "  <<arp.fdepth(i,j)
-      //   <<"    "<<dmdapack.fdepth_vec[i][j]<<std::endl; std::cout<<" i "<<i<<" j "<<j<<" at creation ksat "
-      //   <<arp.ksat(i,j)     <<"    "<<dmdapack.ksat_vec[i][j]<<std::endl; std::cout<<" i "<<i<<" j "<<j<<" at
-      //   creation porosity "<<arp.porosity(i,j) <<"    "<<dmdapack.porosity[i][j]<<std::endl;
     }
   }
 }
