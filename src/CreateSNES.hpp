45753faddd356c4150f729483eb2c20ac7489200
KCallaghan <calla350@umn.edu>
2022-08-30 11:43:01 -0400

    modify arrays available in dmdapack

--- src/CreateSNES.hpp
+++ src/CreateSNES.hpp
@@ -12,7 +12,6 @@ struct AppCtx {
   DM da                   = nullptr;
   Vec x                   = nullptr;  // Solution vector
   Vec b                   = nullptr;  // RHS vector
-  Vec storativity_vec     = nullptr;
   Vec cellsize_EW_squared = nullptr;
   Vec fdepth_vec          = nullptr;
   Vec ksat_vec            = nullptr;
@@ -22,13 +21,13 @@ struct AppCtx {
   Vec T_vec               = nullptr;
   Vec head                = nullptr;
   Vec porosity_vec        = nullptr;
+  Vec starting_wtd        = nullptr;
 
   ~AppCtx() {
     SNESDestroy(&snes);
     DMDestroy(&da);
     VecDestroy(&x);
     VecDestroy(&b);
-    VecDestroy(&storativity_vec);
     VecDestroy(&cellsize_EW_squared);
     VecDestroy(&fdepth_vec);
     VecDestroy(&ksat_vec);
@@ -38,6 +37,7 @@ struct AppCtx {
     VecDestroy(&T_vec);
     VecDestroy(&head);
     VecDestroy(&porosity_vec);
+    VecDestroy(&starting_wtd);
   }
 
   // Extract global vectors from DM; then duplicate for remaining
@@ -45,7 +45,6 @@ struct AppCtx {
   void make_global_vectors() {
     DMCreateGlobalVector(da, &x);
     VecDuplicate(x, &b);
-    VecDuplicate(x, &storativity_vec);
     VecDuplicate(x, &cellsize_EW_squared);
     VecDuplicate(x, &fdepth_vec);
     VecDuplicate(x, &ksat_vec);
@@ -55,6 +54,7 @@ struct AppCtx {
     VecDuplicate(x, &T_vec);
     VecDuplicate(x, &head);
     VecDuplicate(x, &porosity_vec);
+    VecDuplicate(x, &starting_wtd);
   }
 };
 
--- src/DMDA_array_pack.hpp
+++ src/DMDA_array_pack.hpp
@@ -2,7 +2,6 @@
 
 struct DMDA_Array_Pack {
   PetscScalar** x                   = nullptr;
-  PetscScalar** storativity_vec     = nullptr;
   PetscScalar** cellsize_EW_squared = nullptr;
   PetscScalar** fdepth_vec          = nullptr;
   PetscScalar** ksat_vec            = nullptr;
@@ -12,13 +11,13 @@ struct DMDA_Array_Pack {
   PetscScalar** T_vec               = nullptr;
   PetscScalar** head                = nullptr;
   PetscScalar** porosity_vec        = nullptr;
+  PetscScalar** starting_wtd        = nullptr;
   const AppCtx* context             = nullptr;
 
   DMDA_Array_Pack(const AppCtx& user) {
     assert(!context);  // Make sure we're not already initialized
     context = &user;
     DMDAVecGetArray(user.da, user.x, &x);
-    DMDAVecGetArray(user.da, user.storativity_vec, &storativity_vec);
     DMDAVecGetArray(user.da, user.cellsize_EW_squared, &cellsize_EW_squared);
     DMDAVecGetArray(user.da, user.fdepth_vec, &fdepth_vec);
     DMDAVecGetArray(user.da, user.ksat_vec, &ksat_vec);
@@ -28,12 +27,12 @@ struct DMDA_Array_Pack {
     DMDAVecGetArray(user.da, user.T_vec, &T_vec);
     DMDAVecGetArray(user.da, user.head, &head);
     DMDAVecGetArray(user.da, user.porosity_vec, &porosity_vec);
+    DMDAVecGetArray(user.da, user.starting_wtd, &starting_wtd);
   }
 
   void release() {
     assert(context);  // Make sure we are already initialized
     DMDAVecRestoreArray(context->da, context->x, &x);
-    DMDAVecRestoreArray(context->da, context->storativity_vec, &storativity_vec);
     DMDAVecRestoreArray(context->da, context->cellsize_EW_squared, &cellsize_EW_squared);
     DMDAVecRestoreArray(context->da, context->fdepth_vec, &fdepth_vec);
     DMDAVecRestoreArray(context->da, context->ksat_vec, &ksat_vec);
@@ -43,6 +42,7 @@ struct DMDA_Array_Pack {
     DMDAVecRestoreArray(context->da, context->T_vec, &T_vec);
     DMDAVecRestoreArray(context->da, context->head, &head);
     DMDAVecRestoreArray(context->da, context->porosity_vec, &porosity_vec);
+    DMDAVecRestoreArray(context->da, context->starting_wtd, &starting_wtd);
     context = nullptr;
   }
 };
